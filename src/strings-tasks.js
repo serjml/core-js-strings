8f629d013877dd7235d09070993dab7e0638d3ea
Serj Lyakhov <serjml@list.ru>
2024-11-09 10:50:11 +0300

    feat: complete the sumOfCodes task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -192,8 +192,17 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sum = 0;
+
+  if (!str) {
+    return 0;
+  }
+
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
