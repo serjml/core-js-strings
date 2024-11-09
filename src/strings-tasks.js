00e61face0141d4f2a7b6e89a631c7b2b9a8f248
Serj Lyakhov <serjml@list.ru>
2024-11-09 11:53:09 +0300

    feat: complete the findLongestWord task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -357,8 +357,8 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence.split(' ').sort((a, b) => b.length - a.length)[0];
 }
 
 /**
