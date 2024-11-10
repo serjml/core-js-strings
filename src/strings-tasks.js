ab99bd89dd62d123d9b85f072d27bba8c9f17cdb
Serj Lyakhov <serjml@list.ru>
2024-11-10 13:37:40 +0300

    feat: complete the getCardId task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -528,8 +528,32 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const rank = value.slice(0, -1);
+  const suit = value.slice(-1);
+  const ranks = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const suits = {
+    '♣': 0,
+    '♦': 13,
+    '♥': 26,
+    '♠': 39,
+  };
+
+  return ranks.indexOf(rank) + suits[suit];
 }
 
 module.exports = {
